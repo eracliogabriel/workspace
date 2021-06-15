f466fe4c18208898b1cc22ae2ae0a428bcf55c24
eracliogabriel <lopes.biel@hotmailcom>
2021-06-14 22:02:00 -0300

    Atualizando as linhas de codigo

--- Atividade04/src/Questao04.java
+++ Atividade04/src/Questao04.java
@@ -1,18 +1,15 @@
 
 import javax.swing.JOptionPane;
 
-public class Questao04 extends JOptionPane {
+public class Questao04 {
 
 	public static void main(String[] args) {
 
 				String acesso = JOptionPane.showInputDialog("Digite a palavra de acesso");
 				
-				if(acesso.equals("Java-2021"))
-				{
+				if(acesso.equals("Java-2021")) {
 					JOptionPane.showMessageDialog(null, "Permissão: Aprovada");
-				}
-				else 
-				{
+				} else {
 				for (int tentativas=5; tentativas >=1; tentativas--) {
 						JOptionPane.showInputDialog("Tente novamente. Você tem: " +tentativas+ " tentativas");
 					}
