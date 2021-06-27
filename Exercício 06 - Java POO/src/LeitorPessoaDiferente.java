f98a2e28a123cd2a32c17c83405fc8b51a263e64
eracliogabriel <lopes.biel@hotmailcom>
2021-06-27 15:33:38 -0300

    Aplicando correção do exercicio06 e AlgoritmoCpf

--- Exercício 06 - Java POO/src/LeitorPessoaDiferente.java
+++ Exercício 06 - Java POO/src/LeitorPessoaDiferente.java
@@ -1,15 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import javax.swing.JOptionPane;
 
 public class LeitorPessoaDiferente {
 	public static void main(String[] args) {
 		
 		PessoaDiferente P1 = new PessoaDiferente();	
-		
+		DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");	
 		P1.setNome(JOptionPane.showInputDialog("Digite aqui o nome da pessoa:"));
 		P1.setAltura(JOptionPane.showInputDialog("Digite aqui a altura da pessoa:"));
-		P1.setDiaNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite aqui o dia do aniversário dessa pessoa:")));
-		P1.setMesNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite aqui o mês do aniversário dessa pessoa:")));
-		P1.setAnoNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite aqui o ano do aniversário dessa pessoa:")));
+		P1.setDtNascimento(LocalDate.parse(JOptionPane.showInputDialog("Digite aqui a data de nascimento: "),dataFormatada));
+//		P1.setDiaNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite aqui o dia do aniversário dessa pessoa:")));
+//		P1.setMesNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite aqui o mês do aniversário dessa pessoa:")));
+//		P1.setAnoNascimento(Integer.parseInt(JOptionPane.showInputDialog("Digite aqui o ano do aniversário dessa pessoa:")));
 				JOptionPane.showMessageDialog(null, P1.dados());				
 	}
 }
--- Exercício 06 - Java POO/src/PessoaDiferente.java
+++ Exercício 06 - Java POO/src/PessoaDiferente.java
@@ -1,24 +1,32 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class PessoaDiferente {
 	private String nome;
 	private String altura;
-	private int diaNascimento;
-	private int mesNascimento;
-	private int anoNascimento;
+//	private int diaNascimento;
+//	private int mesNascimento;
+//	private int anoNascimento;
+	private LocalDate dtNascimento;
 	
 	public String dados(){
+		DateTimeFormatter dataFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		return "*** DADOS PESSOAIS ***" 
 			 + "\n" + "Nome: " + getNome()
-			 + "\n" + "Data de Nascimento: " +getDiaNascimento()+"/"+getMesNascimento()+"/"+getAnoNascimento()
+			 + "\n" + "Data de Nascimento: "+getDtNascimento().format(dataFormatada)
 			 + "\n" + "Idade: " + idade()
 			 + "\n" + "Altura: " + getAltura()+"m";
 	}
 	public int idade() {
 		LocalDate dataatual = LocalDate.now();
-		int anoatual = dataatual.getYear();
-		int idade = anoatual - getAnoNascimento();
+		int idade = dataatual.getYear() - getDtNascimento().getYear();
+		
+		if(getDtNascimento().getMonthValue() < dataatual.getMonthValue() || (getDtNascimento().getMonthValue() == dataatual.getMonthValue() && getDtNascimento().getDayOfMonth() > dataatual.getDayOfMonth())) {
+		}
 		return idade;
+//		int anoatual = dataatual.getYear();
+//		int idade = anoatual - getAnoNascimento();
+//		return idade;
 	}
 	public String getNome() {
 		return nome;
@@ -32,22 +40,28 @@ public class PessoaDiferente {
 	public void setAltura(String altura) {
 		this.altura = altura;
 	}
-	public int getDiaNascimento() {
-		return diaNascimento;
+//	public int getDiaNascimento() {
+//		return diaNascimento;
+//	}
+//	public void setDiaNascimento(int diaNascimento) {
+//		this.diaNascimento = diaNascimento;
+//	}
+//	public int getMesNascimento() {
+//		return mesNascimento;
+//	}
+//	public void setMesNascimento(int mesNascimento) {
+//		this.mesNascimento = mesNascimento;
+//	}
+//	public int getAnoNascimento() {
+//		return anoNascimento;
+//	}
+//	public void setAnoNascimento(int anoNascimento) {
+//		this.anoNascimento = anoNascimento;
+//	}
+	public LocalDate getDtNascimento() {
+		return dtNascimento;
 	}
-	public void setDiaNascimento(int diaNascimento) {
-		this.diaNascimento = diaNascimento;
-	}
-	public int getMesNascimento() {
-		return mesNascimento;
-	}
-	public void setMesNascimento(int mesNascimento) {
-		this.mesNascimento = mesNascimento;
-	}
-	public int getAnoNascimento() {
-		return anoNascimento;
-	}
-	public void setAnoNascimento(int anoNascimento) {
-		this.anoNascimento = anoNascimento;
+	public void setDtNascimento(LocalDate dtNascimento) {
+		this.dtNascimento = dtNascimento;
 	}
 }
